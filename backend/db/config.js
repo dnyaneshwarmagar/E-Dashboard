d3a6e6ea3f82d04a756290e2d241f01042efa190
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2023-11-22 13:02:48 +0530

    Update config.js

--- backend/db/config.js
+++ backend/db/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = () => {
     try {
-        return mongoose.connect("mongodb+srv://dnyaneshm:magar123@cluster0.jc4sy.mongodb.net/dashboard?retryWrites=true&w=majority");
+        return mongoose.connect("mongodb+srv://dnyaneshm:<password>@cluster0.jc4sy.mongodb.net/dashboard?retryWrites=true&w=majority");
     } catch (err) {
         console.log(err)
     }
